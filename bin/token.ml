8e453e74b30cf6847ecd2bd71b9962147509b27d
vitor <vi.souza.almeida@protonmail.com>
2024-11-30 11:09:34 -0300

    style: improve OCaml code formatting and organization (#1)
    - Format code

--- bin/token.ml
+++ bin/token.ml
@@ -1,5 +1,6 @@
-module Token = struct 
+module Token = struct
   include Fmt
+
   type token_type =
     | ILLEGAL
     | EOF
@@ -33,64 +34,56 @@ module Token = struct
     | EQ
     | NOT_EQ
 
-      type token = {
-        t_type : token_type;
-        literal: string;
-      }
+  type token = { t_type : token_type; literal : string }
 
+  let token_to_string = function
+    | ILLEGAL -> "ILLEGAL"
+    | EOF -> "EOF"
+    | IDENT a -> "IDENT " ^ a
+    | INT a -> "INT " ^ a
+    | ASSIGN -> "ASSIGN"
+    | PLUS -> "PLUS"
+    | COMMA -> "COMMA"
+    | SEMICOLON -> "SEMICOLON"
+    | LPAREN -> "LPAREN"
+    | RPAREN -> "RPAREN"
+    | LBRACE -> "LBRACE"
+    | RBRACE -> "RBRACE"
+    | FUNCTION -> "FUNCTION"
+    | LET -> "LET"
+    | MINUS -> "MINUS"
+    | BANG -> "BANG"
+    | ASTERISK -> "ASTERISK"
+    | SLASH -> "SLASH"
+    | LT -> "LT"
+    | GT -> "GT"
+    | TRUE -> "TRUE"
+    | FALSE -> "FALSE"
+    | IF -> "IF"
+    | ELSE -> "ELSE"
+    | RETURN -> "RETURN"
+    | EQ -> "EQ"
+    | NOT_EQ -> "NOT_EQ"
 
-      let token_to_string = function
-      | ILLEGAL -> "ILLEGAL"
-      | EOF -> "EOF"
-      | IDENT a -> "IDENT " ^ a
-      | INT a -> "INT " ^ a
-      | ASSIGN -> "ASSIGN"
-      | PLUS -> "PLUS"
-      | COMMA -> "COMMA"
-      | SEMICOLON -> "SEMICOLON"
-      | LPAREN -> "LPAREN"
-      | RPAREN -> "RPAREN"
-      | LBRACE -> "LBRACE"
-      | RBRACE -> "RBRACE"
-      | FUNCTION -> "FUNCTION"
-      | LET -> "LET"
-      | MINUS -> "MINUS"
-      | BANG -> "BANG"
-      | ASTERISK -> "ASTERISK"
-      | SLASH -> "SLASH"
-      | LT -> "LT"
-      | GT -> "GT"
-      | TRUE -> "TRUE"
-      | FALSE -> "FALSE"
-      | IF -> "IF"
-      | ELSE -> "ELSE"
-      | RETURN -> "RETURN"
-      | EQ -> "EQ"
-      | NOT_EQ -> "NOT_EQ"
+  let string_to_token =
+    [
+      ("fn", FUNCTION);
+      ("let", LET);
+      ("if", IF);
+      ("else", ELSE);
+      ("true", TRUE);
+      ("false", FALSE);
+      ("return", RETURN);
+    ]
 
-  let string_to_token = [
-    "fn", FUNCTION;
-    "let", LET;
-    "if", IF;
-    "else", ELSE;
-    "true", TRUE;
-    "false", FALSE;
-    "return", RETURN;
-  ]
+  let lookupIdent str =
+    try List.assoc str string_to_token with Not_found -> IDENT str
 
-  let lookupIdent str = try
-    List.assoc str string_to_token
-      with Not_found -> IDENT str
+  let tokens_eq tok_a tok_b =
+    match (tok_a, tok_b) with
+    | IDENT a, IDENT b -> a = b
+    | tok_a, tok_b -> tok_a = tok_b
 
-  let tokens_eq tok_a tok_b = match (tok_a, tok_b) with 
-      | IDENT a, IDENT b -> a = b
-      | tok_a, tok_b -> tok_a = tok_b
-
-      let pretty_print ppf tok = Fmt.pf ppf "Token %s" (token_to_string tok)
-
-  let new_token  tok_type ch = {
-    t_type = tok_type;
-    literal = String.make 1 ch;
-  }
-
+  let pretty_print ppf tok = Fmt.pf ppf "Token %s" (token_to_string tok)
+  let new_token tok_type ch = { t_type = tok_type; literal = String.make 1 ch }
 end
--- test/test.ml
+++ test/test.ml
@@ -1,6 +1,7 @@
 open Monkey
 include Lexer
 include Token
+
 (* A module with functions to test *)
 let () =
   Alcotest.(check (list int))
@@ -10,142 +11,129 @@ let () =
 let token_testable = Alcotest.testable Token.pretty_print Token.tokens_eq
 
 let source_test_string =
-  "let five = 5;
-
-  let ten = 10;
-
-  let add = fn(x, y) {
-    x + y;
-  };
-
-  let result = add(five, ten);
-
-	!-/*5;
-	5 < 10 > 5;
-
-	if (5 < 10) {
-		return true;
-	} else {
-		return false;
-	}
-
-
-	10 == 10;
-	10 != 9;
-  "
+  "let five = 5;\n\n\
+  \  let ten = 10;\n\n\
+  \  let add = fn(x, y) {\n\
+  \    x + y;\n\
+  \  };\n\n\
+  \  let result = add(five, ten);\n\n\
+   \t!-/*5;\n\
+   \t5 < 10 > 5;\n\n\
+   \tif (5 < 10) {\n\
+   \t\treturn true;\n\
+   \t} else {\n\
+   \t\treturn false;\n\
+   \t}\n\n\n\
+   \t10 == 10;\n\
+   \t10 != 9;\n\
+  \  "
 
 let source_test_expected =
   [
     (* // let five = 5 *)
-		Token.LET;
-		Token.IDENT "five";
-		Token.ASSIGN;
-		Token.INT "5";
-		Token.SEMICOLON;
-
-		(* // let ten = 10 *)
-		Token.LET;
-		Token.IDENT "ten";
-		Token.ASSIGN;
-		Token.INT "10";
-		Token.SEMICOLON;
-
-		(* // function definition *)
-		Token.LET;
-		Token.IDENT "add";
-		Token.ASSIGN;
-		Token.FUNCTION;
-		Token.LPAREN;
-		Token.IDENT "x";
-		Token.COMMA;
-		Token.IDENT "y";
-		Token.RPAREN;
-		Token.LBRACE;
-		Token.IDENT "x";
-		Token.PLUS;
-		Token.IDENT "y";
-		Token.SEMICOLON;
-		Token.RBRACE;
-		Token.SEMICOLON;
-
-		(* // result definition *)
-		Token.LET;
-		Token.IDENT "result";
-		Token.ASSIGN;
-		Token.IDENT "add";
-		Token.LPAREN;
-		Token.IDENT "five";
-		Token.COMMA;
-		Token.IDENT "ten";
-		Token.RPAREN;
-		Token.SEMICOLON;
-
-		(* // junk *)
-		Token.BANG;
-		Token.MINUS;
-		Token.SLASH;
-		Token.ASTERISK;
-		Token.INT "5";
-		Token.SEMICOLON;
-		Token.INT "5";
-		Token.LT;
-		Token.INT "10";
-		Token.GT;
-		Token.INT "5";
-		Token.SEMICOLON;
-
-		(* // if/else/return *)
-		Token.IF;
-		Token.LPAREN;
-		Token.INT "5";
-		Token.LT;
-		Token.INT "10";
-		Token.RPAREN;
-		Token.LBRACE;
-		Token.RETURN;
-		Token.TRUE;
-		Token.SEMICOLON;
-		Token.RBRACE;
-		Token.ELSE;
-		Token.LBRACE;
-		Token.RETURN;
-		Token.FALSE;
-		Token.SEMICOLON;
-		Token.RBRACE;
-
-		(* // double character operators *)
-		Token.INT "10";
-		Token.EQ;
-		Token.INT "10";
-		Token.SEMICOLON;
-		Token.INT "10";
-		Token.NOT_EQ;
-		Token.INT "9";
-		Token.SEMICOLON;
+    Token.LET;
+    Token.IDENT "five";
+    Token.ASSIGN;
+    Token.INT "5";
+    Token.SEMICOLON;
+    (* // let ten = 10 *)
+    Token.LET;
+    Token.IDENT "ten";
+    Token.ASSIGN;
+    Token.INT "10";
+    Token.SEMICOLON;
+    (* // function definition *)
+    Token.LET;
+    Token.IDENT "add";
+    Token.ASSIGN;
+    Token.FUNCTION;
+    Token.LPAREN;
+    Token.IDENT "x";
+    Token.COMMA;
+    Token.IDENT "y";
+    Token.RPAREN;
+    Token.LBRACE;
+    Token.IDENT "x";
+    Token.PLUS;
+    Token.IDENT "y";
+    Token.SEMICOLON;
+    Token.RBRACE;
+    Token.SEMICOLON;
+    (* // result definition *)
+    Token.LET;
+    Token.IDENT "result";
+    Token.ASSIGN;
+    Token.IDENT "add";
+    Token.LPAREN;
+    Token.IDENT "five";
+    Token.COMMA;
+    Token.IDENT "ten";
+    Token.RPAREN;
+    Token.SEMICOLON;
+    (* // junk *)
+    Token.BANG;
+    Token.MINUS;
+    Token.SLASH;
+    Token.ASTERISK;
+    Token.INT "5";
+    Token.SEMICOLON;
+    Token.INT "5";
+    Token.LT;
+    Token.INT "10";
+    Token.GT;
+    Token.INT "5";
+    Token.SEMICOLON;
+    (* // if/else/return *)
+    Token.IF;
+    Token.LPAREN;
+    Token.INT "5";
+    Token.LT;
+    Token.INT "10";
+    Token.RPAREN;
+    Token.LBRACE;
+    Token.RETURN;
+    Token.TRUE;
+    Token.SEMICOLON;
+    Token.RBRACE;
+    Token.ELSE;
+    Token.LBRACE;
+    Token.RETURN;
+    Token.FALSE;
+    Token.SEMICOLON;
+    Token.RBRACE;
+    (* // double character operators *)
+    Token.INT "10";
+    Token.EQ;
+    Token.INT "10";
+    Token.SEMICOLON;
+    Token.INT "10";
+    Token.NOT_EQ;
+    Token.INT "9";
+    Token.SEMICOLON;
     (* sentinel *)
     Token.EOF;
   ]
 
 let test_lexer_delimiters () =
   Alcotest.(check (list token_testable))
-    "same token types" [
-        Token.ASSIGN
-      ; Token.PLUS
-      ; Token.LPAREN
-      ; Token.RPAREN
-      ; Token.LBRACE
-      ; Token.RBRACE
-      ; Token.COMMA
-      ; Token.SEMICOLON
-      ; Token.EOF
-      ]
-      (Lexer.generate_tokens "=+(){},;")
-
+    "same token types"
+    [
+      Token.ASSIGN;
+      Token.PLUS;
+      Token.LPAREN;
+      Token.RPAREN;
+      Token.LBRACE;
+      Token.RBRACE;
+      Token.COMMA;
+      Token.SEMICOLON;
+      Token.EOF;
+    ]
+    (Lexer.generate_tokens "=+(){},;")
 
 let test_lexer_source () =
   Alcotest.(check (list token_testable))
     "source code can lex" source_test_expected
-      (Lexer.generate_tokens source_test_string)
+    (Lexer.generate_tokens source_test_string)
 
 (* Run it *)
 let () =
@@ -153,7 +141,6 @@ let () =
     [
       ( "list-delimiters",
         [ Alcotest.test_case "first case" `Slow test_lexer_delimiters ] );
-
       ( "Lexer.generate_tokens",
-        [ Alcotest.test_case "source code" `Slow test_lexer_source ] )
+        [ Alcotest.test_case "source code" `Slow test_lexer_source ] );
     ]
